5289d336e14af48c139d8408e6b3dea5cfbd4e8a
Victor Lastre <rockzila2011@gmail.com>
2026-02-27 15:41:48 -0300

    sistemas bug arreglado

--- backend-rol/index.js
+++ backend-rol/index.js
@@ -21,12 +21,16 @@ const authRoutes = require('./routes/authRoutes');
 const eventosRoutes = require('./routes/eventosRoutes');
 const partidasRoutes = require('./routes/partidasRoutes');
 const usuariosRoutes = require('./routes/usuariosRoutes');
+// ✨ AQUÍ IMPORTAMOS EL NUEVO PERGAMINO DE SISTEMAS
+const sistemasRoutes = require('./routes/sistemas'); // Asegúrate de que el archivo se llame sistemas.js (o sistemasRoutes.js si seguiste ese patrón)
 
 // Usamos las rutas
 app.use('/api', authRoutes); // /api/registro y /api/login
 app.use('/api/eventos', eventosRoutes);
 app.use('/api/partidas', partidasRoutes);
 app.use('/api/usuarios', usuariosRoutes); // Cambiamos /api/admin y /api/mis-cronicas a esta ruta
+// ✨ AQUÍ LE DECIMOS AL SERVIDOR QUE ABRA LAS PUERTAS A LOS SISTEMAS
+app.use('/api/sistemas', sistemasRoutes);
 
 const PUERTO = process.env.PORT || 3001;
 app.listen(PUERTO, '0.0.0.0', () => {
